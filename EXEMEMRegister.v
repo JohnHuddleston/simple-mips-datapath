c10ceb7be405f2d3ebe10637713bf402a27c9feb
John <jdh5658@psu.edu>
2017-04-14 21:39:42 -0400

    Added inter-stage registers

---
+++ EXEMEMRegister.v
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////////
+//										
+// EXE/MEM register module								
+// 										
+//////////////////////////////////////////////////////////////////////////////////
+
+module EXEMEMRegister(
+	input clk, wregin, m2regin, wmemin,
+	input [4:0] RdRtin,
+	input [31:0] aluresultin, qbin,
+	output reg wregout, m2regout, wmemout,
+	output reg [4:0] RdRtout,
+	output reg [31:0] aluresultout, qbout
+	);
+
+always @ (posedge clk) begin
+	wregout = wregin;
+	m2regout = m2regin;
+	wmemout = wmemin;
+	RdRtout = RdRtin;
+	aluresultout = aluresultin;
+	qbout = qbin;
+end
---
+++ MEMWBRegister.v
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+//										
+// MEM/WB register module								
+// 										
+//////////////////////////////////////////////////////////////////////////////////
+
+module MEMWBRegister(
+	input clk, wregin, m2regin,
+	input [4:0] RdRtin,
+	input [31:0] aluresultin, memdatain,
+	output reg wregout, m2regout,
+	output reg [4:0] RdRtout,
+	output reg [31:0] aluresultout, memdataout
+	);
+
+always @ (posedge clk) begin
+	wregout = wregin;
+	m2regout = m2regin;
+	RdRtin = RdRtout;
+	aluresultout = aluresultin;
+	memdataout = memdatain;
+end
